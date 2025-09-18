9d41742c00bbced5ffbfe9f5cb6c191cc281bf6e
natan-felipe <128814253+natantxt@users.noreply.github.com>
2025-09-18 03:10:32 -0300

    Update models.py

--- catalogo/models.py
+++ catalogo/models.py
@@ -11,7 +11,6 @@ class Catalogo(ImageHandlerMixin, models.Model):
     empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE, related_name="catalogos", null=True, blank=True)
     # Se a categoria for deletada, o catálogo não será, ele apenas ficará "sem categoria", o que é mais seguro.
     categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, blank=True, related_name="catalogos")
-<<<<<<< HEAD
     nome = models.CharField(max_length=60, help_text="Ex: Coleção Verão 2025, Mercearia, Roupas, Sobremesas...")
     descricao = models.CharField(max_length=80, blank=True, null=True, help_text="Diga brevemente o que o catálogo aborda (se necessário)")
     #Arquivo em BLOB (no django ele fica BinaryField, porque blob é Binary Large Object, assim, o arquivo passa a armazenar os dados brutos da imagem diretamente no banco de dados, em vez de salvar o arquivo em uma pasta e apenas guardar o caminho até ele)
