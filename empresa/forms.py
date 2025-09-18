9117f4486f084ea4a44a3eea5720e39d4f66a863
natan-felipe <128814253+natantxt@users.noreply.github.com>
2025-09-18 01:48:54 -0300

    Update forms.py

--- empresa/forms.py
+++ empresa/forms.py
@@ -4,11 +4,4 @@ from .models import Empresa
 class EmpresaForm(forms.ModelForm):
     class Meta:
         model = Empresa
-<<<<<<< HEAD
         fields = ('nome', 'CNPJ', 'estado', 'cidade', 'bairro', 'logradouro', 'numero', 'detalhamento','ramo')
-        #
-=======
-        fields = ('nome_loja', 'cnpj', 'estado', 'cidade', 'bairro', 'logradouro', 'numero', 'detalhamento','ramo')
-
-    
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
