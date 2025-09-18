23a4f42de8af9bde0ffe51efdad22126406c7305
natan-felipe <128814253+natantxt@users.noreply.github.com>
2025-09-18 02:02:28 -0300

    Delete Vitrine/catalogo directory

--- Vitrine/catalogo/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Catalogo
-
-class CatalogoForm(forms.ModelForm):
-    class Meta:
-        model = Catalogo
-        # 'categoria' entra aqui, pois o usuário precisa escolher.
-        fields = ('thumbnail', 'título', 'descrição', 'categoria')
