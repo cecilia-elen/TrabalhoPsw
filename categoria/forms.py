d8f505c400ea96e6af55ab63adf8ac796f04e1a9
natan-felipe <128814253+natantxt@users.noreply.github.com>
2025-09-18 01:52:42 -0300

    Update forms.py

--- categoria/forms.py
+++ categoria/forms.py
@@ -4,17 +4,9 @@ from .models import Categoria
 class CategoriaForm(forms.ModelForm):
     class Meta:
         model = Categoria
-<<<<<<< HEAD
         fields = ('nome', 'cat_pai')
-=======
-        fields = ('nome', 'categoria_pai')
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # Este trecho torna o campo 'categoria_pai' opcional no formulário
-      
-<<<<<<< HEAD
-#
-=======
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
+
