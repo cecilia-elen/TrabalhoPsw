db520153cf04d66b4c66e125ff7cacce25bb400e
natan-felipe <128814253+natantxt@users.noreply.github.com>
2025-09-18 02:11:58 -0300

    Update views.py

--- usuario/views.py
+++ usuario/views.py
@@ -1,23 +1,11 @@
 from django.urls import reverse
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 60e0c3e232c3b9522bac92c41a6682a34c1e7220
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages #para essa parte de avisos de sucesso ou erro e tals
 from django.contrib.auth.decorators import login_required, permission_required
 from usuario.models import Usuario
-<<<<<<< HEAD
 from django.contrib.auth import get_user_model
 from .forms import UsuarioForm, UsuarioUpdateForm, LoginForm, AdminUsuarioUpdateForm
-=======
-from .forms import UsuarioForm, LoginForm
-from .forms import UsuarioForm, UsuarioUpdateForm, LoginForm, AdminUsuarioForm
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
 
 def cadastro(request):
     if request.method == 'POST':
@@ -28,41 +16,12 @@ def cadastro(request):
             login(request, usuario)
             # Redireciona o novo usuário para completar o perfil!
             return redirect(reverse('usuario:perfil'))
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
-from .forms import UsuarioForm, LoginForm
-
-def cadastro(request):
-    if request.method == 'POST':
-        form = UsuarioForm(request.POST)
-        if form.is_valid():
-            # Se estiver tudo certo com o formulário, ele vai salvar e vai fazer login
-            usuario = form.save()
-            login(request, usuario)
-             # Redirecionamento para página inicial
-            return redirect(reverse('pagina-inicial')) 
->>>>>>> 1cea6da5e9c6ae1a5fcfbe83fecbbd074ab1453d
->>>>>>> 60e0c3e232c3b9522bac92c41a6682a34c1e7220
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
     else:
         form = UsuarioForm()
 
     context = {'form': form}
     return render(request, 'usuario/cadastro.html', context)
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 60e0c3e232c3b9522bac92c41a6682a34c1e7220
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
 
 
 
@@ -70,7 +29,6 @@ def cadastro(request):
 
 @login_required
 def editar_perfil(request):
-<<<<<<< HEAD
     perfil_do_usuario = request.user.usuario
 
     if request.method == 'POST':
@@ -90,38 +48,12 @@ def editar_perfil(request):
 
     contexto = {'form': form}
     return render(request, 'usuario/editar_perfil.html', contexto)
-=======
-    if request.method == 'POST':
-        # Lembrar do request.FILES para a foto
-        form = UsuarioUpdateForm(request.POST, request.FILES, instance=request.user)
-        if form.is_valid():
-            form.save()
-            # Redireciona para a página de perfil para ver as alterações
-            return redirect(reverse('usuario:perfil'))
-
-    else:
-        # Preenche o formulário com os dados atuais do usuário
-        form = UsuarioUpdateForm(instance=request.user)
-
-    context = {'form': form}
-    # Precisamos de um novo template para esta página
-    return render(request, 'usuario/editar_perfil.html', context)
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
 
 
 
 
 
 
-
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 1cea6da5e9c6ae1a5fcfbe83fecbbd074ab1453d
->>>>>>> 60e0c3e232c3b9522bac92c41a6682a34c1e7220
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
 def login_user(request):
      # Se o usuário já está logado, redireciona para a página inicial
     if request.user.is_authenticated:
@@ -141,32 +73,13 @@ def login_user(request):
 
     return render(request, 'usuario/login.html', context)
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 60e0c3e232c3b9522bac92c41a6682a34c1e7220
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
 
-
-
-
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 1cea6da5e9c6ae1a5fcfbe83fecbbd074ab1453d
->>>>>>> 60e0c3e232c3b9522bac92c41a6682a34c1e7220
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
 def logout_user(request):
     logout(request)
 
     return redirect(reverse('pagina-inicial'))
  #Desloga o usuário e o redireciona para a página inicial.
 
-<<<<<<< HEAD
 
 @login_required
 def perfil(request, pk=None):
@@ -191,35 +104,6 @@ def perfil(request, pk=None):
 
 
 
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 60e0c3e232c3b9522bac92c41a6682a34c1e7220
-
-
-
-
-
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 1cea6da5e9c6ae1a5fcfbe83fecbbd074ab1453d
->>>>>>> 60e0c3e232c3b9522bac92c41a6682a34c1e7220
-@login_required
-def perfil(request):
-    """Renderiza a página de perfil do usuário logado."""
-    # Rodar o Perfil do usuário com suas informações basiconas
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 60e0c3e232c3b9522bac92c41a6682a34c1e7220
-    return render(request, 'usuario/perfil.html')
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
-
-
-
-
 
 @login_required
 def excluir_usuario(request):
@@ -238,12 +122,7 @@ def excluir_usuario(request):
 
 
 
-<<<<<<< HEAD
-=======
 
-
-
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
 @login_required
 def visualizar_usuario(request):
     #Renderiza a página com todos os detalhes do perfil do usuário logado.
@@ -257,21 +136,13 @@ def visualizar_usuario(request):
 @login_required
 @permission_required('usuario.view_usuario', raise_exception=True)
 def listar_usuarios(request):
-<<<<<<< HEAD
     # Buscamos os objetos Usuario
-=======
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
     usuarios = Usuario.objects.all().order_by('first_name')
     return render(request, 'usuario/listar_usuarios.html', {'usuarios': usuarios})
 
 
 
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
 @login_required
 @permission_required('usuario.delete_usuario', raise_exception=True)
 def excluir_usuario_admin(request, pk):
@@ -285,31 +156,17 @@ def excluir_usuario_admin(request, pk):
         if request.user == usuario_alvo:
             messages.error(request, "Você não pode excluir sua própria conta de administrador a partir daqui.")
             return redirect('usuario:listar_usuarios')
-<<<<<<< HEAD
         nome_alvo = usuario_alvo.get_full_name() or usuario_alvo.username # Guarda o nome para a mensagem de sucesso
-=======
-        nome_alvo = usuario_alvo.first_name # Guarda o nome para a mensagem de sucesso
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
         usuario_alvo.delete()
         messages.success(request, f'O usuário "{nome_alvo}" foi excluído com sucesso.')
         return redirect('usuario:listar_usuarios')
 
     # Prepara o contexto para a página de confirmação
-<<<<<<< HEAD
     contexto = {'item_a_excluir': usuario_alvo, 'tipo': 'Usuário'}
     return render(request, 'usuario/excluir_confirmacao.html', contexto)
 
 
-=======
-    contexto = {'item': usuario_alvo, 'tipo': 'Usuário'}
-    return render(request, 'usuario/excluir_confirmacao.html', contexto)
 
-
-
-
-
-
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
 @login_required
 @permission_required('usuario.change_usuario', raise_exception=True)
 def alterar_usuario_admin(request, pk):
@@ -319,7 +176,6 @@ def alterar_usuario_admin(request, pk):
 
     if request.method == 'POST':
         # Cria o formulário preenchido com os dados enviados e os arquivos
-<<<<<<< HEAD
         form = AdminUsuarioUpdateForm(request.POST, request.FILES, instance=usuario_alvo)
         if form.is_valid():
             # Prepara o objeto usuario com os dados do form, mas não salva no banco ainda
@@ -330,16 +186,6 @@ def alterar_usuario_admin(request, pk):
     else:
         # Usa o formulário AdminUsuarioUpdateForm para exibir os dados.
         form = AdminUsuarioUpdateForm(instance=usuario_alvo)
-=======
-        form = AdminUsuarioForm(request.POST, instance=usuario_alvo)
-        if form.is_valid():
-            form.save()
-            messages.success(request, f'Os dados do usuário "{usuario_alvo.username}" foram atualizados com sucesso.')
-            return redirect('usuario:listar_usuarios') # Corrigido para incluir o namespace 'usuario'
-    else:
-        # Usa o formulário AdminUsuarioForm para exibir os dados.
-        form = AdminUsuarioForm(instance=usuario_alvo)
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
 
     #Cria o contexto para enviar as variáveis para o template.
     contexto = {
@@ -349,10 +195,8 @@ def alterar_usuario_admin(request, pk):
 
     return render(request, 'usuario/alterar_usuario_admin.html', contexto)
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
+
 @login_required
 @permission_required('usuario.add_usuario', raise_exception=True)
 def criar_usuario_admin(request):
@@ -367,15 +211,3 @@ def criar_usuario_admin(request):
         form = UsuarioForm()
 
     return render(request, 'usuario/criar_usuario_admin.html', {'form': form})
-<<<<<<< HEAD
-#
-=======
-<<<<<<< HEAD
-    
-=======
-    
-=======
-    return render(request, 'usuario/perfil.html')
->>>>>>> 1cea6da5e9c6ae1a5fcfbe83fecbbd074ab1453d
->>>>>>> 60e0c3e232c3b9522bac92c41a6682a34c1e7220
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
