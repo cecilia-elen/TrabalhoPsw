66d1d7ac71a6cdf275fd6e6c17e78e8e4a794c5e
natan-felipe <128814253+natantxt@users.noreply.github.com>
2025-09-18 02:18:20 -0300

    Update models.py

--- usuario/models.py
+++ usuario/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-<<<<<<< HEAD
 from django.contrib.auth.models import User 
 from django.core.validators import MinValueValidator, MaxValueValidator #valor mínimo e máximo de inteiros para o número das casas
 from Vitrine.mixins import ImageHandlerMixin
@@ -9,65 +8,6 @@ class Usuario(User, ImageHandlerMixin):
     cpf = models.CharField(max_length=14,unique=True,verbose_name='CPF',help_text='Informe o CPF no formato 000.000.000-00')
      #Arquivo em BLOB (no django ele fica BinaryField, porque blob é Binary Large Object, assim, o arquivo passa a armazenar os dados brutos da imagem diretamente no banco de dados, em vez de salvar o arquivo em uma pasta e apenas guardar o caminho até ele)
     foto_perfil = models.BinaryField(blank=True, null=True, verbose_name="Imagem Binária", editable=True)
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 60e0c3e232c3b9522bac92c41a6682a34c1e7220
-from django.contrib.auth.models import AbstractUser, BaseUserManager #É pra que o super user funcione direito, porque o email vira o username na minha costumizaçao de user do django 
-from django.core.validators import MinValueValidator, MaxValueValidator #valor mínimo e máximo de inteiros para o número das casas
-from django.db.models.signals import post_save
-
-class UserManager(BaseUserManager):
-
-    #Manager customizado para o modelo Usuario onde o email é o identificador únicoem vez do username.
-
-    def create_user(self, email, password, **extra_fields):
-        if not email:
-            raise ValueError('O Email é um campo obrigatório')
-        email = self.normalize_email(email)
-        # Define o username para ser igual ao email, garantindo que o campo não fique vazio
-        username = email
-        user = self.model(username=username, email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('O superuser deve ter is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('O superuser deve ter is_superuser=True.')
-        
-        return self.create_user(email, password, **extra_fields)
-    
-class Usuario(AbstractUser):
-<<<<<<< HEAD
-=======
-=======
-from django.contrib.auth.models import AbstractUser
-
-class Usuario(AbstractUser):
-
->>>>>>> 1cea6da5e9c6ae1a5fcfbe83fecbbd074ab1453d
->>>>>>> 60e0c3e232c3b9522bac92c41a6682a34c1e7220
-    email = models.EmailField(unique=True)
-     # Diz ao Django que o campo de login agora é o 'email'
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 60e0c3e232c3b9522bac92c41a6682a34c1e7220
-
-    objects = UserManager()
-    cpf = models.CharField(max_length=14,unique=True,verbose_name='CPF',help_text='Informe o CPF no formato 000.000.000-00')
-    foto_perfil = models.ImageField(upload_to='fotos_perfil/', null=True, blank=True)
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
     sobre = models.TextField(verbose_name='Sobre', blank=True, null=True, help_text='Coloque aqui informações sobre você, slogan, um texto atrativo... como quiser!')
     estado = models.CharField(max_length=50, blank=True, null=True)
     cidade = models.CharField(max_length=50, blank=True, null=True)
@@ -88,33 +28,8 @@ class Usuario(AbstractUser):
     Tiktok = models.URLField(max_length=200, null=True, blank=True)
     Telegram = models.URLField(max_length=200, null=True, blank=True)
     Whatsapp = models.URLField(max_length=200, null=True, blank=True)
-<<<<<<< HEAD
     image_field_name = 'foto_perfil'
 
-
-=======
-
-
-<<<<<<< HEAD
-=======
-=======
-    cpf = models.CharField(max_length=14,unique=True,verbose_name='CPF',help_text='Informe o CPF no formato 000.000.000-00')
->>>>>>> 1cea6da5e9c6ae1a5fcfbe83fecbbd074ab1453d
->>>>>>> 60e0c3e232c3b9522bac92c41a6682a34c1e7220
-
-    def __str__(self):
-        return self.email # Isso ajuda a ver o email do usuário no painel admin
-
-    class Meta:
-        permissions = [
-            ('detalha_pessoa', 'Pode ver detalhes de uma pessoa em específico')
-        ]
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 60e0c3e232c3b9522bac92c41a6682a34c1e7220
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
-        
     @property
     def perfil_completo(self):
 
@@ -122,12 +37,3 @@ class Usuario(AbstractUser):
         campos_obrigatorios = [self.sobre,self.estado,self.cidade,self.bairro,self.logradouro,self.numero]
         return all(campos_obrigatorios)
     
-<<<<<<< HEAD
-#
-=======
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 1cea6da5e9c6ae1a5fcfbe83fecbbd074ab1453d
->>>>>>> 60e0c3e232c3b9522bac92c41a6682a34c1e7220
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
