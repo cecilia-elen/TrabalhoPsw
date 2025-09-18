8e41c3b0e48a06a5da89db56afe73f5b0e8d506c
natan-felipe <128814253+natantxt@users.noreply.github.com>
2025-09-18 01:36:03 -0300

    Update forms.py

--- produto/forms.py
+++ produto/forms.py
@@ -1,13 +1,9 @@
 from django import forms
-<<<<<<< HEAD
 from django.core.files.uploadedfile import UploadedFile #pra verificar o tipo do arquivo importado
-=======
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
 from .models import Produto, Catalogo_Produto
 from empresa.models import Empresa
 
 class ProdutoForm(forms.ModelForm):
-<<<<<<< HEAD
     IMG = forms.ImageField(label="Imagem do Produto", required=False, widget=forms.ClearableFileInput)
     class Meta:
         model = Produto
@@ -22,11 +18,10 @@ class ProdutoForm(forms.ModelForm):
         if commit:
             instance.save()
         return instance
-=======
+
     class Meta:
         model = Produto
         fields = ('nome', 'unidade', 'preco', 'descricao', 'imagem', 'categoria', 'empresa')
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
 
         # Ele garante que o dropdown 'empresa' só mostre as empresas do usuário logado.
     def __init__(self, *args, **kwargs):
@@ -34,29 +29,15 @@ class ProdutoForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         
         if user:
-<<<<<<< HEAD
             self.fields['empresa'].queryset = Empresa.objects.filter(responsavel=user)
-
-=======
-            self.fields['empresa'].queryset = Empresa.objects.filter(usuario=user)
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
 
 class Catalogo_ProdutoForm(forms.ModelForm):
     class Meta:
         model = Catalogo_Produto
-<<<<<<< HEAD
         fields = ('produto', 'preco')
-=======
-        fields = ('produto', 'preco_promocional')
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
 
     def __init__(self, empresa, *args, **kwargs):
         #Este método especial customiza o formulário. Ele garante que o campo 'produto' só mostre os produtos do usuário que é dono do catálogo, evitando que uma loja adicione produtos de outra por engano.
         super().__init__(*args, **kwargs)
         if empresa:
             self.fields['produto'].queryset = Produto.objects.filter(empresa=empresa)   
-<<<<<<< HEAD
-    #
-=======
-    
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
