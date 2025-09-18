60eb883b9e346882a3ea2a7b205b54dec55490b3
natan-felipe <128814253+natantxt@users.noreply.github.com>
2025-09-18 01:52:56 -0300

    Update apps.py

--- categoria/apps.py
+++ categoria/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class CategoriaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'categoria'
-<<<<<<< HEAD
-#
-=======
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
