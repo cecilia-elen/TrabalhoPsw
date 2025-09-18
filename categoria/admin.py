2d5e3346454ce7985a515ba9066f2e595fa5ca60
natan-felipe <128814253+natantxt@users.noreply.github.com>
2025-09-18 01:53:59 -0300

    Update admin.py

--- categoria/admin.py
+++ categoria/admin.py
@@ -1,26 +1,2 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 60e0c3e232c3b9522bac92c41a6682a34c1e7220
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
-#from django.contrib import admin
-#from .models import Categoria
-
-# Esta linha "mágica" registra o modelo Categoria no painel admin
-<<<<<<< HEAD
-#admin.site.register(Categoria)
-#
-=======
-<<<<<<< HEAD
-#admin.site.register(Categoria)
-=======
-#admin.site.register(Categoria)
-=======
 from django.contrib import admin
 
-# Register your models here.
->>>>>>> 1cea6da5e9c6ae1a5fcfbe83fecbbd074ab1453d
->>>>>>> 60e0c3e232c3b9522bac92c41a6682a34c1e7220
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
