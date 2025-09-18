1243a371fd1916723ae24cb00fe07beb9badf74a
natan-felipe <128814253+natantxt@users.noreply.github.com>
2025-09-18 01:42:51 -0300

    Update apps.py

--- produto/apps.py
+++ produto/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class ProdutoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'produto'
-<<<<<<< HEAD
-#
-=======
->>>>>>> 8750437900de8beadd7384be3adc93d797d65d08
